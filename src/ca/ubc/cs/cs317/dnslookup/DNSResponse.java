6be45645466faa67f1866505ac87218137874a27
Daniel Le <l9k0b@ugrad.cs.ubc.ca>
2018-11-01 16:06:43 -0700

    Resolved merge conflicts.

--- src/ca/ubc/cs/cs317/dnslookup/DNSResponse.java
+++ src/ca/ubc/cs/cs317/dnslookup/DNSResponse.java
@@ -10,81 +10,10 @@ public class DNSResponse {
 	public byte[] additionals;
 
 	public DNSResponse(byte[] data) {
-<<<<<<< HEAD
-		int offset = 0;
-		// GET HEADER
-		for (int i = 0; i < 12; i++) {
-			header[i] = data[i];
-		}
-		offset = 12;
-		
-		// GET QUESTIONS
-		if (data[offset] == 0xc0) {
-			offset = offset+6;
-			question = new byte[6];
-			for (int i = 0; i < 6; i++) {
-				question[i] = data[offset+i];
-			}
-		} else {
-			for (int j = 0; j < getByteInt(getNumQuestion()); j++) {
-				int qsize = 0;
-				while (data[offset+qsize] != 0x00) {
-					qsize++;
-				}
-				qsize++;
-				qsize = qsize+4;
-				question = new byte[qsize];
-				for (int i = 0; i < qsize; i++) {
-					question[i] = data[offset+i];
-				}
-				offset += qsize;
-			}
-		}
-		
-		// GET ANSWERS
-		if (data[offset] == 0xc0) {
-			offset = offset+10;
-			byte[] rbuf = new byte[2];
-			rbuf[0] = data[offset];
-			offset++;
-			rbuf[1] = data[offset];
-			offset++;
-			int responseSize = getByteInt(rbuf);
-			question = new byte[12+responseSize];
-			for (int i = 0; i < 16; i++) {
-				question[i] = data[offset+i];
-			}
-		} else {
-			for (int j = 0; j < getByteInt(getNumAnswer()); j++) {
-				int qsize = 0;
-				while (data[offset+qsize] != 0x00) {
-					qsize++;
-				}
-				qsize++;
-				qsize = qsize+8;
-				byte[] rbuf = new byte[2];
-				rbuf[0] = data[offset+qsize];
-				qsize++;
-				rbuf[1] = data[offset+qsize];
-				qsize++;
-				int responseSize = getByteInt(rbuf);
-				question = new byte[qsize];
-				for (int i = 0; i < qsize; i++) {
-					question[i] = data[offset+i];
-				}
-				offset += qsize;
-			}
-		}
-	}
-	
-	int getByteInt(byte[] bytes) {
-	     return ByteBuffer.wrap(bytes).getInt();
-=======
 		parseHeader(data);
 		parseAnswer(data);
 		parseAuth(data);
 		parseAdditionals(data);
->>>>>>> parsing
 	}
 
 	private void parseHeader(byte[] data) {
@@ -102,8 +31,8 @@ public class DNSResponse {
 		this.additionals = additionals;
 	}
 
-	public getNumberAuthoritatives() {
-
+	public int getNumberAuthoritatives() {
+		return 0;
 	}
 
 }
